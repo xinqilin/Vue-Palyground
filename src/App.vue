60238f62f5d49a90b9fa0ec1731c2cb3752286d0
BillLin1209 <zzx123bill@gmail.com>
2021-12-19 20:57:14 +0800

    gmail app with vue3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <h1>VMail Inbox</h1>
 
-
   <Suspense>
     <template #default>
       <MailTable />
@@ -14,10 +13,17 @@
 
 <script>
 import MailTable from '@/components/MailTable.vue';
+import useEmailSelection from '@/composables/use-email-selection';
+
 export default {
   name: 'App',
   components: {
     MailTable
+  },
+  setup(){
+    return {
+      emailSelection: useEmailSelection()
+    }
   }
 };
 </script>
--- src/components/MailTable.vue
+++ src/components/MailTable.vue
@@ -1,42 +1,63 @@
 <template>
+  <button @click="selectedScreen = 'inbox'"
+          :disabled="selectedScreen == 'inbox'">Inbox</button>
+  <button @click="selectedScreen = 'archive'"
+          :disabled="selectedScreen == 'archive'">Archived</button>
+
+  <BulkActionBar :emails="filteredEmails" />
+
   <table class="mail-table">
     <tbody>
-      <tr v-for="email in unarchivedEmails"
+      <tr v-for="email in filteredEmails"
           :key="email.id"
-          :class="['clickable', email.read ? 'read' : '']"
-          @click="openEmail(email)">
+          :class="['clickable', email.read ? 'read' : '']">
         <td>
-          <input type="checkbox" />
+          <input type="checkbox"
+                 @click="emailSelection.toggle(email)"
+                 :checked="emailSelection.emails.has(email)" />
         </td>
-        <td>{{email.from}}</td>
-        <td>
+        <td @click="openEmail(email)">{{email.from}}</td>
+        <td @click="openEmail(email)">
           <p><strong>{{email.subject}}</strong> - {{email.body}}</p>
         </td>
-        <td class="date">{{format(new Date(email.sentAt), 'MMM do yyyy')}}</td>
+        <td class="date" @click="openEmail(email)">
+          {{format(new Date(email.sentAt), 'MMM do yyyy')}}
+        </td>
         <td><button @click="archiveEmail(email)">Archive</button></td>
       </tr>
     </tbody>
   </table>
-  <MailView v-if="openedEmail" :email="openedEmail" />
+
+  <ModalView v-if="openedEmail" @closeModal="openedEmail = null">
+    <MailView :email="openedEmail" @changeEmail="changeEmail" />
+  </ModalView>
 </template>
 
 <script>
   import { format } from 'date-fns';
   import axios from 'axios';
   import MailView from '@/components/MailView.vue';
-  import { ref } from 'vue';
+  import ModalView from '@/components/ModalView.vue';
+  import BulkActionBar from '@/components/BulkActionBar.vue';
+  import { reactive, ref } from 'vue';
+  import useEmailSelection from '@/composables/use-email-selection'
 
   export default {
     async setup(){
       let {data: emails} = await axios.get('http://localhost:3000/emails')
+
       return {
+        emailSelection: useEmailSelection(),
         format,
         emails: ref(emails),
-        openedEmail: ref(null)
+        openedEmail: ref(null),
+        selectedScreen: ref('inbox')
       }
     },
     components: {
-      MailView
+      MailView,
+      ModalView,
+      BulkActionBar,
     },
     computed: {
       sortedEmails() {
@@ -44,19 +65,44 @@
           return e1.sentAt < e2.sentAt ? 1 : -1
         })
       },
-      unarchivedEmails() {
-        return this.sortedEmails.filter(e => !e.archived)
+      filteredEmails() {
+        if(this.selectedScreen == 'inbox') {
+          return this.sortedEmails.filter(e => !e.archived)
+        } else {
+          return this.sortedEmails.filter(e => e.archived)
+        }
       }
     },
     methods: {
+      selectScreen(newScreen) {
+        this.selectedScreen = newScreen
+        this.emailSelection.clear()
+      },
       openEmail(email) {
-        email.read = true
-        this.updateEmail(email)
         this.openedEmail = email
+
+        if(email) {
+          email.read = true
+          this.updateEmail(email)
+        }
       },
       archiveEmail(email) {
         email.archived = true
         this.updateEmail(email)
+      },
+      changeEmail({toggleRead, toggleArchive, save, closeModal, changeIndex}) {
+        let email = this.openedEmail
+        if(toggleRead) { email.read = !email.read }
+        if(toggleArchive) { email.archived = !email.archived }
+        if(save) { this.updateEmail(email) }
+        if(closeModal) { this.openedEmail = null }
+
+        if(changeIndex) {
+          let emails = this.filteredEmails
+          let currentIndex = emails.indexOf(this.openedEmail)
+          let newEmail = emails[currentIndex + changeIndex]
+          this.openEmail(newEmail)
+        }
       },
       updateEmail(email) {
         axios.put(`http://localhost:3000/emails/${email.id}`, email)
